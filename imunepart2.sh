c3cd79ecbf561296368e4e85c02c8b5e4fe2b6ba
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-04-23 02:01:33 +0700

    Add files via upload

---
+++ imunepart2.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+echo
+sleep 1
+echo $blue"#########################################"
+echo $yellow"# (+) AUTHOR : MR VIRUS SPM         (+) #"
+echo $yellow"# (+) CNL YT : MR VIRUS SPM NEW     (+) #"
+echo $yellow"# (+) SCRIPT : WHATSAP IMUNE PART 2 (+) #"
+echo $blue"#########################################"
+echo
+echo
+sleep 1
+xdg-open https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ
+echo $red
+read -p "MASUKAN PASWORD DULU DONGS ==> " dux
+if [ $dux = 4321 ]
+then
+echo
+sleep 1
+echo $purple"[ sudah subcribe belum ......... 5% ]"
+sleep 2
+echo $green"[ mohon bersabar tunggu ........ 25% ]"
+sleep 2
+echo $cyan"[ masih dalam proses ........... 75% ]"
+sleep 2
+echo $white"[ lima detik lagi geng ......... 100% ]"
+sleep 3
+echo
+xdg-open https://www.mediafire.com/file/ahi4l9aglkw3tij/GB_WhatsApp4.apk/file
+fi
